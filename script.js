1578f46c74b64aec2a97fcaa19c3cdccbb54cac4
Tia-Koenig <tiakoenig1994@gmail.com>
2021-07-14 20:11:37 +1000

    created styling sheets for html pages, created aboutme page. rounded images in blog html & about me page

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const hamburger_menu = document.querySelector(".hamburger-menu")
-const hamburger_menu_close = document.querySelector(".hamburger-menu-close")
-const navbar = document.querySelector("#navbar")
+const hamburger_menu = document.querySelector('.hamburger-menu');
+const hamburger_menu_close = document.querySelector('.hamburger-menu-close');
+const navbar = document.querySelector('#navbar');
+const body = document.querySelector('body');
 
-function toggle_navbar(){
-  navbar.classList.toggle("navbar-open")
-  hamburger_menu.classList.toggle("active")
-  hamburger_menu_close.classList.toggle("active")
+function toggle_navbar() {
+  navbar.classList.toggle('navbar-open');
+  hamburger_menu.classList.toggle('active');
+  hamburger_menu_close.classList.toggle('active');
 }
 
-hamburger_menu.addEventListener("click", function(){
-  toggle_navbar()
-})
+hamburger_menu.addEventListener('click', function () {
+  toggle_navbar();
+});
 
-hamburger_menu_close.addEventListener("click", () => {
-  toggle_navbar()
-})
-
+hamburger_menu_close.addEventListener('click', () => {
+  toggle_navbar();
+});
